da7b3cfbe9d601827b1210f01bd8e9a834757bf4
fbdev1 <syrkinvictor@gmail.com>
2021-04-21 15:00:32 +0500

    adding .gitignore an creation new branch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat = (Cat)applicationContext.getBean("cat");
+        Cat beanCat2 =(Cat)applicationContext.getBean("cat");
+        System.out.print("Сравнение двух бинов ПриветМир: ");
+        System.out.println(bean == bean2);
+        System.out.print("Сравнение двух бинов Cat: ");
+        System.out.println(beanCat == beanCat2);
+
     }
 }
